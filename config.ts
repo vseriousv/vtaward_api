432fdbee5ae10f7c67c3b205112c7a5bf8b20bcc
Vladimir Mikhaylov <zombotv82@gmail.com>
2020-05-01 18:10:56 +0700

    fix env

--- config.ts
+++ config.ts
@@ -1,4 +1,6 @@
 import { config as configDev } from './config/config.development';
 import { config as configProd } from './config/config.production';
+import env from 'dotenv';
+env.config();
 
 export default process.env.NODE_ENV === 'production' ? configProd : configDev;
--- config/config.production.ts
+++ config/config.production.ts
@@ -1,10 +1,12 @@
 import { Dialect } from 'sequelize/types';
+import env from 'dotenv';
+env.config();
 
 export const config = {
     database: {
         dialect: 'postgres' as Dialect,
         host: process.env.DATABASE_HOST,
-        port: +process.env.DATABASE_PORT,
+        port: process.env.DATABASE_PORT,
         username: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_DATABASE,
