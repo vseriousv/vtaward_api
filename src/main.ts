024f1c34f8d422d22270610074eafe4eb556ad70
Kirill S Gavrilov <gavrilovskirill@gmail.com>
2020-05-12 19:39:48 +0700

    Fixed config cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors(
+    {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+      allowedHeaders: '*',
+    }
+  );
   app.useGlobalPipes(new ValidationPipe());
   setupSwagger(app);
   await app.listen(3000);
