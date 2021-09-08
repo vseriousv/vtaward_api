7ed5afa639cf2d5636c819b8491437b3c02fc4d5
vseriousv <gavrilovskirill@gmail.com>
2021-09-08 09:08:32 +0300

    fix: move run params to envs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './swagger';
 
+const {RUN_HOST, RUN_PORT} = process.env;
+const host = RUN_HOST || '127.0.0.1';
+const port = +RUN_PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   // app.enableCors(
@@ -15,7 +19,7 @@ async function bootstrap() {
   // );
   app.useGlobalPipes(new ValidationPipe());
   setupSwagger(app);
-  await app.listen(3000);
+  await app.listen(port, host);
 }
 
 bootstrap();
